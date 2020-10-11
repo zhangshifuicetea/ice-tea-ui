79dd985ecad17ae58c0316284bf816a0b22a3f7f
zhangshifu <tiejiayrz@outlook.com>
2020-10-12 00:42:08 +0800

    add examples

---
+++ examples/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Icon} from '../lib';
+
+const IconExample: React.FC = () => {
+    return (
+        <>
+            <Icon name={'wechat'}/>
+            <Icon name={'alipay'}/>
+        </>
+    );
+};
+
+export default IconExample;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,14 @@
 const base = require('./webpack.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = Object.assign({}, base, {
     mode: "development",
-    // entry: {
-    //     example: './example.tsx',
-    // },
+    entry: {
+        example: path.resolve(__dirname, `examples`, `index.tsx`),
+    },
     plugins: [...base.plugins, new HtmlWebpackPlugin({
         title: 'Ice Tea UI - React',
-        template: 'index.html'
+        template: path.resolve(__dirname, `examples`, `index.html`)
     })],
 });
