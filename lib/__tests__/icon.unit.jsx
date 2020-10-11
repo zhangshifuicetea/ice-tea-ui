6e08d3570a0d2cb1e9743a02192dc456de9b0331
zhangshifu <tiejiayrz@outlook.com>
2020-10-11 18:44:44 +0800

    add test

--- lib/__tests__/icon.unit.jsx
+++ lib/__tests__/icon.unit.jsx
@@ -4,7 +4,7 @@ import Icon from '../icon/icon';
 import {mount} from 'enzyme';
 
 describe('icon', () => {
-    it('snapshot', () => {
+    it('render', () => {
         const json = renderer.create(<Icon name={'wechat'}/>).toJSON();
         expect(json).toMatchSnapshot();
     });
