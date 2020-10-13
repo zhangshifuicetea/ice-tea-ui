ea751b044276212a474344dd692fad99c5641d07
zhangshifu <tiejiayrz@outlook.com>
2020-10-13 18:45:28 +0800

    update export

--- lib/index.tsx
+++ lib/index.tsx
@@ -24,3 +24,5 @@ declare global {
 }
 
 export {default as Icon} from './icon/icon';
+export {default as Dialog, createDialog, confirm} from './dialog/dialog';
+export {Layout, Content, Aside, Header, Footer} from './layout/layout';
